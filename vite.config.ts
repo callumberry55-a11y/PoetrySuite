ac3893c1d1c90c9ec9053ab705dc61709755e8c3
berrycallum00 <berrycallum00@gmail.com>
2026-01-22 10:22:49 +0000

    fixed package.Json error preventing loading

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
